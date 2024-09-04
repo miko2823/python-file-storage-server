215d25db834ef62df8677c671f74a5e0e8dcaa98
KaoriNakajima <kaori.nakajima@lisse-law.co.jp>
2024-09-04 14:31:45 +0900

    feat: add config settings

--- fs_store/config.py
+++ fs_store/config.py
@@ -1,1 +1,36 @@
-BASE_URL = "http://localhost:8082"
+import os
+
+from pydantic_settings import BaseSettings
+
+
+class GlobalConfig(BaseSettings):
+    ENV_STATE: str = os.getenv("ENV_STATE", "local")
+
+
+class LocalConfig(GlobalConfig):
+    BASE_URL = "http://localhost:8000"
+
+
+class StgConfig(GlobalConfig):
+    BASE_URL = "http://localhost:8000"
+
+
+class ProdConfig(GlobalConfig):
+    BASE_URL = "http://localhost:8000"
+
+
+class FactoryConfig:
+    def __init__(self, env_state: str):
+        self.env_state = env_state
+
+    def __call__(self):
+        if self.env_state == "dev":
+            return LocalConfig()
+        elif self.env_state == "stg":
+            return StgConfig()
+        elif self.env_state == "prod":
+            return ProdConfig()
+        raise ValueError(f"Invalid ENV_STATE: {self.env_state}")
+
+
+settings = FactoryConfig(GlobalConfig().ENV_STATE)()
